eebf8c0bba9ad06c4d5e603a952619c1b7398b5e
gabrielpriegoo <gabrielvpriego@gmail.com>
2025-05-19 20:27:47 -0300

    feat: :sparkles: ensuring validation and type of body schema with zod, and  config nestjs module for variables enviroments.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
