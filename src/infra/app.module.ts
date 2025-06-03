6020e5fd6df944167bf6a44e206e628c080dad3b
gabrielpriegoo <gabrielvpriego@gmail.com>
2025-06-03 09:23:28 -0300

    chore: :boom: implements new files for tests, and changes in files for to improve ready following clean archicture

---
+++ src/infra/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './http/controllers/authenticate.controller';
+import { CreateAccountController } from './http/controllers/create-account.controller';
+import { envSchema } from './env';
+import { PrismaService } from './prisma/prisma.service';
+import { CreateQuestionController } from './http/controllers/create-questions.controller';
+import { FetchRecentsQuestions } from './http/controllers/fetch-recent-questions.controller';
+import { HttpModule } from './http/http.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    HttpModule,
+  ],
+})
+export class AppModule {}
